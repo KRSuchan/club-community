4f44f0631c0ca1c0a439b78ae23ff3a0ab4e9475
= <=>
2024-06-07 10:32:35 +0900

    initial commit

---
+++ src/main/java/web/club_community/ClubCommunityApplication.java
@@ -0,0 +1,13 @@
+package web.club_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubCommunityApplication.class, args);
+	}
+
+}
